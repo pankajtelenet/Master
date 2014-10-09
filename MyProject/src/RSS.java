4e7d61effbd0bcc1f9b703d41e8c4b356ccf9764
pchivate <pchivate@PPC04857.prod.telenet.be>
2014-10-09 15:45:56 +0200

    3rd commit

--- MyProject/src/RSS.java
+++ MyProject/src/RSS.java
@@ -5,7 +5,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;  
 import org.w3c.dom.Node;  
 import org.w3c.dom.NodeList;  
-//Some comment added
+//Some comments added again
   
 public class RSS {  
   
