919bde151f2ea2c9141316ee10d8a7f947857d94
pchivate <pchivate@gmail.com>
2015-04-14 15:11:32 +0530

    Removed comment
    Removed comment

--- MyProject/src/RSS.java
+++ MyProject/src/RSS.java
@@ -7,7 +7,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;  
 //Some comments added again
 //Making some changes
-//Jenkins Test - Testing conflict 2nd comment
+//Jenkins Test
 //Testing auto trigger to Jenkins -- comment
 
   
